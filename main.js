f7caf5c5cfb125edce1c2b17d34b93b8573a22eb
Izzet Poyraz <izzet9719@gmail.com>
2023-03-17 16:17:13 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+(function () {
+    const Hamburger = document.querySelector('.hamburger');
+
+    Hamburger.addEventListener('click', () =>{  
+        Hamburger.classList.toggle('dropdown');
+    })
+  })();
+
