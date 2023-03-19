32a7c0b62930ae069506c55ced8d1e7150d1c6b4
Izzet <izzet9719@gmail.com>
2023-03-19 21:53:43 +0100

    Update js + span

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const toggle = document.getElementsByTagName('nav')[0];
 const hm = document.getElementsByClassName('hamburger')[0];
+const icon = document.getElementsByClassName('icon')[0];
 
 hm.addEventListener('click', () => {
     toggle.classList.toggle('dropdown');
+    icon.classList.toggle('dropdown');
 } )
