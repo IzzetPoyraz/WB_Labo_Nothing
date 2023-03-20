cb7c5c3e11c5bca6d36f528d7a77397aaf581ead
Izzet <izzet9719@gmail.com>
2023-03-20 20:50:17 +0100

    update css

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const toggle = document.getElementsByTagName('nav')[0];
 const hm = document.getElementsByClassName('hamburger')[0];
 const icon = document.getElementsByClassName('icon')[0];
-
 const moreLess = document.getElementById('showMore');
 const gallery = document.getElementsByClassName('gallery')[0];
 const more = document.getElementById('more');
