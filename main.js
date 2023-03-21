6388e432b9c9e973f7c0e957caf446dae76ae917
Izzet <izzet9719@gmail.com>
2023-03-21 20:45:51 +0100

    update

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 'use strict';
 
-const toggle = document.getElementsByTagName('nav')[0];
-const hm = document.getElementsByClassName('hamburger')[0];
-const icon = document.getElementsByClassName('icon')[0];
-const moreLess = document.getElementById('showMore');
-const gallery = document.getElementsByClassName('gallery')[0];
-const more = document.getElementById('more');
-const less = document.getElementById('less');
+(function () {
+    const toggle = document.getElementsByTagName('nav')[0];
+    const hm = document.getElementsByClassName('hamburger')[0];
+    const icon = document.getElementsByClassName('icon')[0];
+    
+    
+    const moreLess = document.getElementById('showMore');
+    const gallery = document.getElementsByClassName('gallery')[0];
+    const more = document.getElementById('more');
+    const less = document.getElementById('less');
+    
+    hm.addEventListener('click', () => {
+        toggle.classList.toggle('dropdown');
+        icon.classList.toggle('dropdown');
+    } );
+    
+    moreLess.addEventListener('click', () => {
+        gallery.classList.toggle('flex');
+        more.classList.toggle('flex');
+        less.classList.toggle('flex');
+    });
+  })();
 
-hm.addEventListener('click', () => {
-    toggle.classList.toggle('dropdown');
-    icon.classList.toggle('dropdown');
-} );
 
-moreLess.addEventListener('click', () => {
-    gallery.classList.toggle('flex');
-    more.classList.toggle('flex');
-    less.classList.toggle('flex');
-});
